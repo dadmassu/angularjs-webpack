54384c84197fd2926a4aef9fecc9c0d96d2330b5
= <dadmassu1920@gmail.com>
2017-03-15 08:17:18 +0100

    This is where the commit message goes

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import starter from './starter/starte.component';
 import beginner from './beginner/beginner.component';
+import angularMaterial from 'angular-material';
+import simple from './starter/simple.component'
 
 import 'angular-animate';
 import 'angular-aria';
@@ -19,13 +21,16 @@ let app = () => {
 class AppCtrl {
     constructor() {
         this.url = 'https://github.com/preboot/angular-webpack';
+        this.what = "what is speaking at the moment"
+        this.names = ['dawit', 'tuna', 'afom'];
     }
 }
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
+angular.module(MODULE_NAME, [angularMaterial])
     .directive('app', app)
+    .component('simpleState', simple)
     .component('starter', starter)
     .component('beginner', beginner)
     .controller('AppCtrl', AppCtrl);
--- src/app/beginner/beginner.component.js
+++ src/app/beginner/beginner.component.js
@@ -1,11 +1,6 @@
-class controller {
-    constructor($scope) {
-        this.$scope = $scope;
-        this.$scope.names = ["first", "second", "third"];
-    }
-}
-
 export default {
     template: "<div><ul><li ng-repeat='name in names'>{{name}}</li></ul></div>",
-    controller,
+    bindings: {
+        names: '=',
+    },
 }
